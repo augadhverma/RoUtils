4b62d470313d2aee093f7802799c1f446633dec0
ItsArtemiz <itsartemiz01@gmail.com>
2021-02-13 13:16:01 +0530

    redoing everything

--- README.md
+++
@@ -1,3 +0,0 @@
-# RoUtils
-
-In writing
--- utils/requests.py
+++
@@ -1,6 +0,0 @@
-import aiohttp
-
-async def get(*args, **kwargs) -> dict:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(*args, **kwargs) as r:
-            return await r.json()
