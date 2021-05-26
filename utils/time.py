5a3a1e040f1159e499b058b850376f55c0535256
ItsArtemiz <itsartemiz01@gmail.com>
2021-05-27 01:28:44 +0530

    Added TimeConverter for mute

--- utils/time.py
+++ utils/time.py
@@ -1,5 +1,8 @@
 import humanize
+import re
+
 from datetime import datetime
+from discord.ext import commands
 
 def human_time(dt:datetime, **options) -> str:
     """Gives a nicely formated date object which is easy to read.
@@ -24,3 +27,22 @@ def human_time(dt:datetime, **options) -> str:
     if dt is None:
         return 'N/A'
     return f"{humanize.precisedelta(datetime.utcnow() - dt, minimum_unit=minimum_unit, suppress=suppress, format=format)} ago"
+
+# Credits to XuaTheGrate and Vexs for the time converted below.
+
+time_regex = re.compile("(?:(\d{1,5})(h|s|m|d))+?")
+time_dict = {"h":3600, "s":1, "m":60, "d":86400}
+
+class TimeConverter(commands.Converter):
+    async def convert(self, ctx, argument):
+        args = argument.lower()
+        matches = re.findall(time_regex, args)
+        time = 0
+        for v, k in matches:
+            try:
+                time += time_dict[k]*float(v)
+            except KeyError:
+                raise commands.BadArgument(f"{k} is an invalid time-key! h/m/s/d are valid!")
+            except ValueError:
+                raise commands.BadArgument(f"{v} is not a number!")
+        return time
