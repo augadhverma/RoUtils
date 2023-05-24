368a765c55b0b61315457a973d973cd2db724956
Augadh Verma <61934158+ItsArtemiz@users.noreply.github.com>
2023-05-24 17:22:19 +0530

    Bot version bump

--- utils/bot.py
+++ utils/bot.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-# version 3.1
+# version 3.1.1
 
 import os
 import aiohttp
